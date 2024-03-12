86168e1c0253f86996a78d7c697462d857fc5401
felix <fperez2020@outlook.com>
2024-03-11 21:26:45 -0400

    Router activity

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,25 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+  let usernames = req.users.map(function(user) {
+    return {id: user.id, username: user.username};
+  });
+  console.log(usernames);
+  res.send(usernames);
+});
+
+router.get('/username/:name', (req, res) => {
+    let name = req.params.name;
+    let searchResults = req.users.filter(function(user) {
+      return user.username === name;
+    });
+  
+    if (searchResults.length > 0) {
+      res.send(searchResults);
+    } else {
+      res.send({error: {message: `${name} not found`, status: 404}});
+    }
+  });
+
+module.exports = router;
